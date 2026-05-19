7ddcd6a1f6d6d7983a33ace88f298025db865f87
jaguar28-crypto <jaguar28@tutamail.com>
2026-05-19 10:53:09 +1000

    Fix image paths - use relative paths instead of absolute paths for GitHub Pages

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Slideshow functionality
 let currentSlide = 0;
 const slides = [
-  { src: '/Star-Wars/media/SW_E1.webp', alt: '1' },
-  { src: '/Star-Wars/media/SW_E2.jpg', alt: '2' },
-  { src: '/Star-Wars/media/SW_E3.jpg', alt: '3' },
-  { src: '/Star-Wars/media/SW_E4.jpg', alt: '4' },
-  { src: '/Star-Wars/media/SW_E5.jpg', alt: '5' },
-  { src: '/Star-Wars/media/SW_E6.jpg', alt: '6' },
-  { src: '/Star-Wars/media/SW_E7.jpg', alt: '7' },
-  { src: '/Star-Wars/media/SW_E8.webp', alt: '8' },
-  { src: '/Star-Wars/media/SW_E9.webp', alt: '9' }
+  { src: 'media/SW_E1.webp', alt: '1' },
+  { src: 'media/SW_E2.jpg', alt: '2' },
+  { src: 'media/SW_E3.jpg', alt: '3' },
+  { src: 'media/SW_E4.jpg', alt: '4' },
+  { src: 'media/SW_E5.jpg', alt: '5' },
+  { src: 'media/SW_E6.jpg', alt: '6' },
+  { src: 'media/SW_E7.jpg', alt: '7' },
+  { src: 'media/SW_E8.webp', alt: '8' },
+  { src: 'media/SW_E9.webp', alt: '9' }
 ];
 
 function showSlide(n) {
